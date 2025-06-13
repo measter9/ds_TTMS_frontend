1828875eb19aeedbfa8b7e2f202f71abcffb18dd
pavilion <jakisfajnyktos@gmail.com>
2025-06-13 10:20:37 +0200

    table, CRUD

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,10 @@
 import { AsyncPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { Contact } from './models/contacts.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -11,5 +14,74 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'front';
+  http = inject(HttpClient);
+  contacts$ = this.getContacts();
+
+  contactName = '';
+  contactNumber = '';
+  isEditing = false;
+  editingContactId = '';
+
+
+  private getContacts(): Observable<Contact[]> {
+    return this.http.get<Contact[]>('https://localhost:7097/api/Contacts')
+  }
+
+  protected removeContact(id:string){
+this.http.delete(`https://localhost:7097/api/Contacts/${id}`).subscribe({
+      next: (res) => {
+        console.log('usunięto kontakt', res);
+        this.contacts$ = this.getContacts();
+        this.contactName = '';
+        this.contactNumber = '';
+      },
+      error: (err) => {
+        console.error('Błąd przy usuwaniu kontaktu:', err);
+      },
+    });
+    
+  
+  }
+  protected addContact(){
+    const newContact = { name: this.contactName, number: this.contactNumber };
+    this.http.post('https://localhost:7097/api/Contacts', newContact).subscribe({
+      next: (res) => {
+        console.log('Dodano kontakt', res);
+        this.contacts$ = this.getContacts();
+        this.contactName = ''; 
+        this.contactNumber = '';
+      },
+      error: (err) => {
+        console.error('Błąd przy dodawaniu kontaktu:', err);
+      },
+    });
+  }
+
+  protected editBtn(id:string){
+    this.isEditing = true;
+    this.editingContactId = id;
+
+    
+  }
+
+
+  protected editContact() {
+    if (!this.editingContactId) return;
+
+    const updatedContact = {
+      id: this.editingContactId,
+      name: this.contactName,
+      number: this.contactNumber,
+    };
+
+    this.http.put(`https://localhost:7097/api/Contacts/${this.editingContactId}`, updatedContact).subscribe({
+      next: () => {
+        this.contacts$ = this.getContacts();
+        this.contactName = ''; 
+        this.contactNumber = '';      },
+        
+      error: (err) => console.error('Błąd przy edycji kontaktu:', err),
+    });
+  }
+  
 }
